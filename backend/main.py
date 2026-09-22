017f8fdd56be6c808364dc444bd44c3921d2769d
Rudaina220 <s-rudaina.salmin@zewailcity.edu.eg>
2026-09-22 20:16:05 +0300

    Deploy

--- backend/main.py
+++ backend/main.py
@@ -41,9 +41,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "ai-life-quest.vercel.app"
     ],
 
-    allow_credentials=True,
+    allow_credentials=False,
 
     allow_methods=["*"],
 
