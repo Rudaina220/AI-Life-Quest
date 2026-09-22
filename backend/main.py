9ca18cb43ff70929e0fda375c89555893d0c8af1
Rudaina220 <s-rudaina.salmin@zewailcity.edu.eg>
2026-09-22 20:37:02 +0300

    Update CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "ai-life-quest.vercel.app"
+        "https://ai-life-quest-git-main-rudy20.vercel.app"
     ],
 
     allow_credentials=True,
